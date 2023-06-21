e0766dc47137dcd8f52ce40d4950931ec8b71d88
truc <ya.fourni@icloud.com>
2023-06-21 16:24:14 +0200

    add crypto

--- js/manga.js
+++ js/manga.js
@@ -13,28 +13,6 @@ var man = document.createElement('p');
 man.innerHTML="Manga";
 divManga.appendChild(man);
 
-const urlManga = 'https://anilistmikilior1v1.p.rapidapi.com/getAnimeList';
-const headerManga = {
-	method: 'POST',
-	headerManga2: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': 'd851300a01mshbd5199c8ec6fa3ap18ade3jsn969e73f40282',
-		'X-RapidAPI-Host': 'Anilistmikilior1V1.p.rapidapi.com'
-	},
-	body: new URLSearchParams({
-		userId: 'Ryu LGC'
-	})
-};
-
-
-function Manga() {
-    // fetch(urlManga, headerManga)
-    // .then((response) => {
-    //     console.log("2");
-    //     return response.json();
-    // }).then((json) => console.log(json))
-}
-
 function displayDivManga() {
     // affiche info
     divStream.style = "display:none;";
